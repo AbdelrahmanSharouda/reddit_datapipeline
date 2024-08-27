7a8e3a33fde697d86d57d2ff312768c2f5861cda
Abdelrahman Shawki <abdelrahman.shawki@talentsarena.net>
2024-08-27 19:32:37 +0300

    adding dags

---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,9 @@
+
+def reddit_pipeline(file_name:str, subreddit:str, time_filter='day', limit=None):
+    # TODO:
+    # connecting to reddit instance
+    instance = connect_reddit()
+    # extraction
+    # transformation
+    # loading to CSV
+    
