ad26a97f2e783f2e35c16db9c9fc81f24e0c1757
Abdelrahman Shawki <abdelrahman.shawki@talentsarena.net>
2024-08-29 16:19:36 +0300

    Added DAGS, stuff to config file.

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,9 +1,12 @@
+from utils.constants import CLIENT_ID, SECRET
+from etls.reddit_etl import connect_reddit, extract_posts
 
 def reddit_pipeline(file_name:str, subreddit:str, time_filter='day', limit=None):
     # TODO:
     # connecting to reddit instance
-    instance = connect_reddit()
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
     # extraction
+    posts = extract_posts(instance, subreddit, time_filter, limit)
     # transformation
     # loading to CSV
     
